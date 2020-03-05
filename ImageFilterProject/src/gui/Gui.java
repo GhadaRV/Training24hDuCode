5c449cf34c60b3e38375867bd559d653684669aa
Ghada Dridi <35421354+GhadaRV@users.noreply.github.com>
2020-03-05 13:54:29 +0100

    hi

--- ImageFilterProject/src/gui/Gui.java
+++ ImageFilterProject/src/gui/Gui.java
@@ -3,13 +3,17 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JPanel;
 import java.awt.FlowLayout;
 import javax.swing.JToolBar;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Gui {
@@ -49,17 +53,21 @@ public class Gui {
 		frame.setBounds(100, 100, 578, 442);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+	
+		JLabel image = new JLabel( new ImageIcon("cat.png"));
 		JPanel panel = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
 		flowLayout.setAlignOnBaseline(true);
 		panel.setBounds(500, 45, 50, 50);
+		
+
 		frame.getContentPane().add(panel);
 		
 		JPanel panel_1 = new JPanel();
 		FlowLayout flowLayout_1 = (FlowLayout) panel_1.getLayout();
 		flowLayout_1.setAlignOnBaseline(true);
 		panel_1.setBounds(500, 107, 50, 50);
+	//	panel_1.add( image, BorderLayout.CENTER);
 		frame.getContentPane().add(panel_1);
 		
 		JPanel panel_2 = new JPanel();
